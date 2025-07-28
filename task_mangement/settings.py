de16546dacb09f33761f60dc47dc6f116b2db2c3
taiyebaafrin <taiyebasadia753@gmail.com>
2025-07-29 03:51:53 +0600

    end last eight

--- task_mangement/settings.py
+++ task_mangement/settings.py
@@ -25,8 +25,11 @@ SECRET_KEY = 'django-insecure-e$(eeg=&@+hdpy1reo)$dsgg&y=jjd9+w^i#c9gia0l)c)m+x7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://127.0.0.1:8000']
 
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Application definition
 
